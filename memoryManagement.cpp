13d4b52f09d863a9c49c1b196e0ace95f52c126c
Jana <janaakamaal2004@gmail.com>
2025-03-17 15:03:27 +0200

    Update memoryManagement.cpp
    Implementing move semantics

--- memoryManagement.cpp
+++ memoryManagement.cpp
@@ -1,27 +1,54 @@
 #include <iostream>
+#include <memory>
+
+struct Subresource {
+  Subresource(){
+    std::cout << "Creating subresource\n";
+  };
+
+  Subresource(const Subresource& Source) {
+    std::cout
+      << "Copying subresource (expensive!)\n";
+  }
+};
 
 struct Resource {
   // Default constructor
-  Resource() {}
+  Resource()
+    : Sub{std::make_unique<Subresource>()} {
+    std::cout << "Creating resource\n";
+  }
 
   // Copy constructor
-  Resource(const Resource& Source) {
+  Resource(const Resource& Source)
+    : Sub{std::make_unique<Subresource>(
+      *Source.Sub)} {
     std::cout << "Copying resource\n";
   }
 
   // Move constructor
-  Resource(Resource&& Source) {
-    std::cout << "Moving resource\n";
-  }
+  Resource(Resource&& Source)
+    : Sub{std::move(Source.Sub)} {
+        std::cout << "Moving resource\n";
+    }
+  std::unique_ptr<Subresource> Sub;
 };
-int main() {
-    std::cout << "Original resource\n";
+
+int main(){
+    std::cout << "Original Resource\n";
     Resource Original;
 
-    std::cout << "\nresource A:\n";
+    std::cout << "\nCopying Original\n";
     Resource A{Original};
 
-    std::cout << "\nresource B:\n";
+    if(Original.Sub.get())
+        std::cout << "Original still has its subresource\n";
+
+    std::cout << "\nMoving Original\n";
     Resource B{std::move(Original)};
+
+    if(!(Original.Sub.get()))
+        std::cout << "Original no longer has its subresource\n";
+    
     return 0;
 }
